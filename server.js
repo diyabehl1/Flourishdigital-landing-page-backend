202e1a5b2aa811e1a1344a4b1509e212263f875c
diyabehl1 <diyabehl2004@gmail.com>
2025-08-20 10:47:20 +0530

    update MONGO_URI with database name

--- server.js
+++ server.js
@@ -98,4 +98,5 @@ const sendNotification = (formData) => {
 
 
 
-app.listen(5000, () => console.log("🚀 Server running on port 5000"));
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => console.log(`🚀 Server running on port ${PORT}`));
